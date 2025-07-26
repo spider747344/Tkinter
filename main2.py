40d56a3ce330746854c14943947581331888dab9
spider747344 <9630003948sri@gmail.com>
2025-07-26 22:20:22 +0530

    main2.py

---
+++ main2.py
@@ -0,0 +1,15 @@
+from tkinter import*
+window=Tk()
+window.title("Event handler")
+window.geometry("100x100")
+
+def handle_keypress(event):
+    print(event.char)
+window.bind("<Key>",handle_keypress)
+def handle_click(event):
+
+    print("\n the button was clicked!")
+button=Button(text="Click me!")
+button.pack()
+button.bind("<Button-1>",handle_click)
+window.mainloop() 
