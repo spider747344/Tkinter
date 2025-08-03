900eb1e7e4b1a2351bbd7b30f7fdfaa4ab3f90f6
spider747344 <9630003948sri@gmail.com>
2025-08-03 23:08:03 +0530

    main4.py

---
+++ main4.py
@@ -0,0 +1,16 @@
+from tkinter import *
+root=Tk()
+root.geometry("400x300")
+root.title("main")
+def topwin():
+   top=Toplevel()
+   top.geometry("180x100")
+   top.title("toplevel")
+   l2=Label(top,text="this is a toplevel window")
+   l2.pack()
+   top.mainloop()
+l=Label(root,text="this is the root window")
+btn=Button(root,text="click here to open another window",command=topwin)
+l.pack()
+btn.pack()
+root.mainloop() 
